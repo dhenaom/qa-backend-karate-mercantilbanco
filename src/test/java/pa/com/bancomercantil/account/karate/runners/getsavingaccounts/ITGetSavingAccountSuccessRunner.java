74e0a69530c9aceb9360c2cc134299581a62aa6e
davidhenaopragma <dhenao_pragma@mercantilbanco.com.pa>
2025-02-13 15:21:52 -0500

    merge test

---
+++ src/test/java/pa/com/bancomercantil/account/karate/runners/getsavingaccounts/ITGetSavingAccountSuccessRunner.java
@@ -0,0 +1,21 @@
+package pa.com.bancomercantil.account.karate.runners.getsavingaccounts;
+
+import com.intuit.karate.junit5.Karate;
+import pa.com.bancomercantil.authentication.karate.utils.ZipUtil;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ITGetSavingAccountSuccessRunner {
+
+            @Karate.Test
+            Karate getSavingAccountSuccessTest() throws IOException {
+                String timestamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
+                String path = "target/karate-reports-"+timestamp;
+                Karate result = Karate.run("classpath:pa/com/bancomercantil/account/karate/datapersistence/features/datapersistencesuccess.feature")
+                        .relativeTo(getClass()).backupReportDir(true).outputCucumberJson(true);
+                ZipUtil.zipDirectory("target/karate-reports", path + ".zip");
+                return result;
+            }
+        }
